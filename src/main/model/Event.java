199620e80870ff901712dcea46bc4c97f478ef4f
Jiayang Dong <jiayangdong@dhcp-206-87-132-186.ubcsecure.wireless.ubc.ca>
2023-11-28 13:24:11 -0800

    Phase 4 Commit. Trying to fix indentation error

--- src/main/model/Event.java
+++ src/main/model/Event.java
@@ -6,7 +6,6 @@ import java.util.Date;
 
 /**
  * Represents an alarm system event.
- * Adapted from AlarmSystem
  */
 public class Event {
     private static final int HASH_CONSTANT = 13;
@@ -16,8 +15,7 @@ public class Event {
     /**
      * Creates an event with the given description
      * and the current date/time stamp.
-     *
-     * @param description a description of the event
+     * @param description  a description of the event
      */
     public Event(String description) {
         dateLogged = Calendar.getInstance().getTime();
@@ -26,8 +24,7 @@ public class Event {
 
     /**
      * Gets the date of this event (includes time).
-     *
-     * @return the date of the event
+     * @return  the date of the event
      */
     public Date getDate() {
         return dateLogged;
@@ -35,22 +32,19 @@ public class Event {
 
     /**
      * Gets the description of this event.
-     *
-     * @return the description of the event
+     * @return  the description of the event
      */
-    public String getDescription() {
+    public String getDescription () {
         return description;
     }
 
     @Override
     public boolean equals(Object other) {
-        if (other == null) {
+        if (other == null)
             return false;
-        }
 
-        if (other.getClass() != this.getClass()) {
+        if (other.getClass() != this.getClass())
             return false;
-        }
 
         Event otherEvent = (Event) other;
 
